8224d1fe1cf275c6563505d62b9b8dc8b39f48cb
Vladymyr Davii <Vladimir.davii@gmail.com>
2017-06-27 16:17:24 +0300

    ready section - clients

--- app/js/common.js
+++ app/js/common.js
@@ -13,6 +13,16 @@ $(document).ready(function(){
 		prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/general/arrow_left.png'>",
     nextArrow:"<img class='a-right control-c next slick-next' src='../img/general/arrow_right.png'>"
 	});
+
+	$('.clients-slider').slick({
+		infinite: false,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: false,
+		dots: false,
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/general/arrow_left.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../img/general/arrow_right.png'>"
+	});
 });
 
 
